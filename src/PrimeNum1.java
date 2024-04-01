5feee365b30469633e675f0960919042b5122e86
Alexey <psychotrance92@yandex.ru>
2024-04-01 22:22:42 +1200

    Second

--- src/PrimeNum1.java
+++ src/PrimeNum1.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class primenum {
+/**
+ * Вычислить и записать в массив первые 10 простых чисел.
+ * Простое число – положительное целое число,
+ * которое делится без остатка лишь на себя и на 1.
+ * 1 не является простым числом.
+ * Вывести в консоль сумму всех элементов полученного массива.
+ */
+public class PrimeNum1 {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0) {
+                array[i] = 2;
+            } else {
+                int addingValue = i == 1 ? 1 : 2;
+                int num = array[i - 1] + addingValue;
+                boolean isPrime = false;
+                while (!isPrime) {
+                    for (int j = i - 1; j >= 0 ; j--) {
+                        if (num % array[j] == 0) {
+                            num += addingValue;// вот где была моя ошибка и я ее очень долго не хотел видеть(((
+                            break;
+                        } else {
+                            isPrime = true;
+                        }
+                    }
+                }
+                if (isPrime) {
+                    array[i] = num;
+                }
+            }
+        }
+        int result = 0;
+        for (int n : array) {
+            System.out.println(n);
+            result += n;
+        }
+        System.out.println(result);
+    }
 }
--- src/PrimeRecursion.java
+++ src/PrimeRecursion.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class PrimeRecursion {
+/**
+ * Вычислить и записать в массив первые 10 простых чисел.
+ * Простое число – положительное целое число,
+ * которое делится без остатка лишь на себя и на 1.
+ * 1 не является простым числом.
+ * Вывести в консоль сумму всех элементов полученного массива.
+ * Нахождение простых чисел и вычисление суммы реализовать,
+ * используя рекурсивные методы.
+ */
+public class PrimeRecursion {
+    public static void main(String[] args) {
+        int[] primeNums = new int[10];
+
+        for (int i = 0; i < primeNums.length; i++) {
+            if (i == 0) {
+                primeNums[i] = findPrime(i, primeNums[i]);
+                System.out.println(primeNums[i]);
+            } else {
+                primeNums[i] = findPrime(i, primeNums[i - 1]);
+                System.out.println(primeNums[i]);
+            }
+        }
+        System.out.println(sumPrimeNums(primeNums, 0, 0));
+    }
+
+    static int findPrime(int index, int num) {
+        int addingValue = index == 1 ? 1 : 2;
+        num += addingValue;
+        if (index == 0 || index == 1) {
+            return num;
+        } else {
+            boolean isPrime = false;
+            while (!isPrime) {
+                for (int i = 2; i <= num / i; i++) {
+                    if ((num % i) == 0) {
+                        return findPrime(i, num);
+                    } else {
+                        isPrime = true;
+                    }
+                }
+            }
+        }
+        return num;
+    }
+
+    static int sumPrimeNums(int[] array, int index, int sum) {
+        if (index < array.length) {
+            sum += array[index];
+            return sumPrimeNums(array, index + 1, sum);
+        } else {
+            return sum;
+        }
+    }
 }
+/*
+Откровенно говоря, решение не до конца мною выдумано
+Очень долго не мог понть, откуда взять нужные делители для проверок
+И наткнулся на решение в книге Шилдта
+Java полное руководство.
+Так что снова не на 100% решение моё(((
+Но всё остальное - вроде сам (но это не точно).
+29.03.2024г.
+ */
