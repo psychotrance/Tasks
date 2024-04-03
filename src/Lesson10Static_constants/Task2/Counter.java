9bcadeb026a5be9348f471688ca74178c5397d57
Alexey <psychotrance92@yandex.ru>
2024-04-03 15:26:40 +1200

    my decision of task2

--- src/Lesson10Static_constants/Task2/Counter.java
+++ src/Lesson10Static_constants/Task2/Counter.java
@@ -1,23 +1,25 @@
 package Lesson10Static_constants.Task2;
 
+/**
+ * реализуйте неизменность поля названия у класса Counter.
+ * Ведь очень странно, если мы можем менять название счетчика по ходу выполнения
+ * программы, не так ли?
+ */
 public class Counter {
-    public final String NAME = "counter";
+    static final String NAME = "counter";
     public int counter;
 
     public Counter() {
         counter = 0;
     }
-
     public Counter(int counter) {
         this.counter = counter;
     }
-
     public int increase(int value) {
         counter += value;
 
         return counter;
     }
-
     public int decrease(int value) {
         counter -= value;
 
